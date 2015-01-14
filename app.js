f890e909f2a2472735a0de311556bae18b0eb344
Nathan Grunzweig <nathan@codefresh.io>
2015-01-14 16:07:00 +0200

    adding full-stack example content

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+// Constants
+var PORT = 9090;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+	console.log('Got Request!!!');
+  	res.send('Hello world we dont need npm!!!\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
